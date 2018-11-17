2b9904824f91d3f950cea064b62c43a393addd4d
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-11-18 00:15:40 +0300

    Add files via upload

---
+++ src/GenerateKeys.java
@@ -0,0 +1,66 @@
+import java.io.File;
+//import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.sound.sampled.AudioFormat.Encoding;
+
+public class GenerateKeys {
+
+	private KeyPairGenerator keyPairGen;
+	private KeyPair keyPair;
+	private PrivateKey privateKey;
+	private PublicKey publicKey;
+	static GenerateKeys generatekeys;
+
+	public GenerateKeys(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
+		this.keyPairGen = KeyPairGenerator.getInstance("RSA");
+		this.keyPairGen.initialize(keylength);
+	}
+
+	public void createKeys() {
+		this.keyPair = this.keyPairGen.generateKeyPair();
+		this.privateKey = keyPair.getPrivate();
+		this.publicKey = keyPair.getPublic();
+	}
+
+	public PrivateKey getPrivateKey() {
+		return this.privateKey;
+	}
+
+	public PublicKey getPublicKey() {
+		return this.publicKey;
+	}
+
+	public void writeToFile(PrintWriter pKey,String path,byte[] key) throws IOException {
+		File f = new File(path);
+		f.getParentFile().mkdirs();
+		String s = key.toString();
+
+		pKey = new PrintWriter(path);
+		pKey.write(s);
+		pKey.flush();
+		pKey.close();
+		
+	}
+	/*public void createPriKey(PrintWriter privateKey) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
+		
+		generatekeys = new GenerateKeys(1024);
+		generatekeys.createKeys();
+		generatekeys.writeToFile(privateKey, main.privateKey, generatekeys.getPublicKey().getEncoded());
+		//generatekeys.writeToFile(publicKey,main.publicKey, generatekeys.getPrivateKey().getEncoded());
+		
+	}*/
+
+	/*public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
+		createFolder();
+
+	}*/
+
+}
