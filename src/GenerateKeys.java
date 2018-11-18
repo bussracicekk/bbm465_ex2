34730350189e9813e04142d32af5ccde9754fc29
Elmas Dağlı <eedagli@gmail.com>
2018-11-18 22:17:44 +0300

    imzalama yapildi

--- src/GenerateKeys.java
+++ src/GenerateKeys.java
@@ -1,12 +1,6 @@
 import java.io.File;
-<<<<<<< HEAD
-//import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-=======
 import java.io.FileOutputStream;
 import java.io.IOException;
->>>>>>> keys
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -14,14 +8,7 @@ import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-<<<<<<< HEAD
-import javax.sound.sampled.AudioFormat.Encoding;
-
 public class GenerateKeys {
-
-=======
-public class GenerateKeys {
->>>>>>> keys
 	private KeyPairGenerator keyPairGen;
 	private KeyPair keyPair;
 	private PrivateKey privateKey;
@@ -47,33 +34,6 @@ public class GenerateKeys {
 		return this.publicKey;
 	}
 
-<<<<<<< HEAD
-	public void writeToFile(PrintWriter pKey,String path,byte[] key) throws IOException {
-		File f = new File(path);
-		f.getParentFile().mkdirs();
-		String s = key.toString();
-
-		pKey = new PrintWriter(path);
-		pKey.write(s);
-		pKey.flush();
-		pKey.close();
-		
-	}
-	/*public void createPriKey(PrintWriter privateKey) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
-		
-		generatekeys = new GenerateKeys(1024);
-		generatekeys.createKeys();
-		generatekeys.writeToFile(privateKey, main.privateKey, generatekeys.getPublicKey().getEncoded());
-		//generatekeys.writeToFile(publicKey,main.publicKey, generatekeys.getPrivateKey().getEncoded());
-		
-	}*/
-
-	/*public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
-		createFolder();
-
-	}*/
-
-=======
 	public void writeToFile(FileOutputStream fs,String path,byte[] key) throws IOException {
 		File f = new File(path);
 		//f.getParentFile().mkdirs();
@@ -85,5 +45,4 @@ public class GenerateKeys {
 		fs.close();
 		
 	}
->>>>>>> keys
 }
